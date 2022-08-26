9e106feba95773d969c77ae18c1cecdad645a0db
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-26 10:28:20 +0300

    init project for apache mq basics

---
+++ src/main/java/com/example/Apache_Activemq/ApacheActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.example.Apache_Activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheActivemqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Apache_Activemq/ApacheActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apache_Activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheActivemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
