b46b7864d1e479d59e9b48c4a988f3585f24c52b
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-26 11:37:46 +0300

    create message&dest, send mesg to queue, close connection

--- src/main/java/com/example/Apache_Activemq/jms/MessageSender.java
+++ src/main/java/com/example/Apache_Activemq/jms/MessageSender.java
@@ -4,9 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
+import javax.jms.*;
 
 @Slf4j
 //Class used to send a message to queue
@@ -22,5 +20,21 @@ public class MessageSender {
         ConnectionFactory connectionFactory =  new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
+
+//        create a session to send/ receive JMS message
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+//        creates queue automatically on the server
+        Destination destination = session.createQueue(queueName);
+//        Message producer for sending the messages to the queue
+        MessageProducer producer = session.createProducer(destination);
+        TextMessage message = session.createTextMessage("Apache AMQ message");
+//        sends the text message
+        producer.send(message);
+
+        log.info("The text message been sent to the queue "+ message.getText());
+
+//        then close the connection
+        connection.close();
     }
 }
