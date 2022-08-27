38c6372b5517320890e7e30c67aebc5c427bce1c
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-27 23:33:22 +0300

    sanitize phone number

--- src/main/java/com/example/Apache_Activemq/utils/Utils.java
+++ src/main/java/com/example/Apache_Activemq/utils/Utils.java
@@ -9,4 +9,23 @@ public class Utils {
         //return new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(new Date());
         return new SimpleDateFormat("yyyyMMddHHmmssSSS", Locale.getDefault()).format(new Date());
     }
+    public String sanitizePhoneNumber(String phone) {
+
+        if (phone.equals("")) {
+            return "";
+        }
+        if (phone.length() < 11 & phone.startsWith("0")) {
+            String p = phone.replaceFirst("^0", "+254-");
+            return p;
+        }
+        if (phone.length() == 13 && phone.startsWith("+254")) {
+            String p = phone.replaceFirst("^+254", "+254-");
+            return p;
+        }
+        if (phone.length() == 12 && phone.startsWith("254")) {
+            String p = phone.replaceFirst("^254", "+254-");
+            return p;
+        }
+        return phone;
+    }
 }
