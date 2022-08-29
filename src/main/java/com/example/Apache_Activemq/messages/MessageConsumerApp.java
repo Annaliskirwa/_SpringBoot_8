474a3690e63e70d312a7d1add30fc8b925704048
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-29 09:53:54 +0300

    message consumer app

--- src/main/java/com/example/Apache_Activemq/messages/MessageConsumerApp.java
+++ src/main/java/com/example/Apache_Activemq/messages/MessageConsumerApp.java
@@ -1,4 +1,19 @@
 package com.example.Apache_Activemq.messages;
 
+import com.example.Apache_Activemq.config.AppConfig;
+import com.example.Apache_Activemq.receiver.MessageReceiver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+@Slf4j
 public class MessageConsumerApp {
+    public static void main(String[]args){
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessageReceiver messageReceiver = (MessageReceiver) context.getBean("messageReceiver");
+        String response = messageReceiver.receiveMessage();
+        log.info("**********The message has been received************" + response);
+        ((AbstractApplicationContext) context).close();
+
+    }
 }
