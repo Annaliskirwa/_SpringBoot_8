7040e423278b6cfda67d09669ad76716e9b47093
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-29 09:03:25 +0300

    defined bean for sending messages

--- src/main/java/com/example/Apache_Activemq/config/MessageConfiguration.java
+++ src/main/java/com/example/Apache_Activemq/config/MessageConfiguration.java
@@ -1,8 +1,14 @@
 package com.example.Apache_Activemq.config;
 
 import com.example.Apache_Activemq.receiver.MessageReceiver;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+import java.util.Arrays;
 
 @Configuration
 public class MessageConfiguration {
@@ -11,4 +17,20 @@ public class MessageConfiguration {
 
     @Autowired
     MessageReceiver messageReceiver;
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(DEFAULT_BROKER_URL);
+        connectionFactory.setTrustedPackages(Arrays.asList("com.example"));
+        return connectionFactory;
+    }
+//    Sending messages
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        template.setDefaultDestinationName(MESSAGE_QUEUE);
+        return template;
+    }
 }
