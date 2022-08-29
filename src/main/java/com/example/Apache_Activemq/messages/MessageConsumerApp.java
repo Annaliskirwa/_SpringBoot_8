03f48e3c73104dcb5a498c5301222fe573243643
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-29 15:04:26 +0300

    message consumer app

--- src/main/java/com/example/Apache_Activemq/messages/MessageConsumerApp.java
+++ src/main/java/com/example/Apache_Activemq/messages/MessageConsumerApp.java
@@ -1,6 +1,7 @@
 package com.example.Apache_Activemq.messages;
 
 import com.example.Apache_Activemq.config.AppConfig;
+import com.example.Apache_Activemq.model.Product;
 import com.example.Apache_Activemq.receiver.MessageReceiver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,7 +9,7 @@ import org.springframework.context.support.AbstractApplicationContext;
 
 @Slf4j
 public class MessageConsumerApp {
-    public static void main(String[]args){
+    public static void mains(String[]args){
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         MessageReceiver messageReceiver = (MessageReceiver) context.getBean("messageReceiver");
         String response = messageReceiver.receiveMessage();
@@ -16,4 +17,17 @@ public class MessageConsumerApp {
         ((AbstractApplicationContext) context).close();
 
     }
+    public static void main(String[] args)
+    {
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+
+        MessageReceiver messageReceiver = (MessageReceiver) context.getBean("messageReceiver");
+        Product product = messageReceiver.receiveMessage1();
+       log.info("Message Received = " + product);
+
+        ((AbstractApplicationContext) context).close();
+    }
+
+
 }
