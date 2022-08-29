e7c119a2ff2f9914ce0fde68b8ca233e081304b3
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-29 14:55:32 +0300

    message producer app

--- src/main/java/com/example/Apache_Activemq/messages/MessageProducerApp.java
+++ src/main/java/com/example/Apache_Activemq/messages/MessageProducerApp.java
@@ -1,6 +1,7 @@
 package com.example.Apache_Activemq.messages;
 
 import com.example.Apache_Activemq.config.AppConfig;
+import com.example.Apache_Activemq.model.Product;
 import com.example.Apache_Activemq.producer.MessageSender;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,11 +9,24 @@ import org.springframework.context.support.AbstractApplicationContext;
 
 @Slf4j
 public class MessageProducerApp {
-    public static  void main(String[]args){
+    public static void mains(String[]args){
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         MessageSender messageSender =  context.getBean(MessageSender.class);
         messageSender.sendMessage("It is a Monday morning");
         log.info("************The message has been sent successfully************");
+        ((AbstractApplicationContext) context).close();
+    }
+    public static void main(String[]args){
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessageSender messageSender = context.getBean(MessageSender.class);
+
+        Product product = new Product();
+        product.setProductId(100);
+        product.setName("Phone");
+        product.setQuantity(10);
+
+        messageSender.sendMessage1(product);
+        log.info("*********the message has been sent successfully***********");
         ((AbstractApplicationContext) context).close();
     }
 }
