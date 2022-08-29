9f92627b4dca0f1b210724f36543f717b9653e3a
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-29 09:45:27 +0300

    message producer app

--- src/main/java/com/example/Apache_Activemq/messages/MessageProducerApp.java
+++ src/main/java/com/example/Apache_Activemq/messages/MessageProducerApp.java
@@ -1,11 +1,18 @@
 package com.example.Apache_Activemq.messages;
 
 import com.example.Apache_Activemq.config.AppConfig;
+import com.example.Apache_Activemq.producer.MessageSender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
+@Slf4j
 public class MessageProducerApp {
     public static  void main(String[]args){
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessageSender messageSender =  context.getBean(MessageSender.class);
+        messageSender.sendMessage("It is a Monday morning");
+        log.info("************The message has been sent successfully************");
+        ((AbstractApplicationContext) context).close();
     }
 }
